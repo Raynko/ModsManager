a27543922ca2e7a294eb6e30abedf4c42146fc8e
KevinGacon <71588720+Raynko@users.noreply.github.com>
2023-09-15 18:14:50 +0200

    BASE

---
+++ docs/JS/Scripts.js
@@ -0,0 +1,12 @@
+function ShowHideForm() {
+    var x = document.getElementById("add-mod-form");
+    var btn = document.getElementById("show-hide-form");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      btn.textContent = "−";
+    } else {
+      x.style.display = "none";
+      btn.textContent = "+";
+    }
+  }
+  

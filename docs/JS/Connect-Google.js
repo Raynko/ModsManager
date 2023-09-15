39524daa9baf3ada0909237c6a16f8f575122c3d
KevinGacon <71588720+Raynko@users.noreply.github.com>
2023-09-15 14:50:04 +0200

    test

--- docs/JS/Connect-Google.js
+++ docs/JS/Connect-Google.js
@@ -9,3 +9,24 @@ document.getElementById('login-button').addEventListener('click', function() {
   
     window.location.href = authUrl;
   });
+
+const { google } = require('googleapis');
+const credentials = require('JSON/client_secret_471223344745-vm46l33eim5l0n4936eus85rvtqk9ti6.apps.googleusercontent.com.json');
+
+const client = new google.auth.OAuth2(
+  credentials.web.client_id,
+  credentials.web.client_secret,
+  credentials.web.redirect_uris[0]
+);
+
+// Configurer le scope pour les autorisations en écriture
+client.scope = 'https://www.googleapis.com/auth/spreadsheets';
+
+// Créer un lien d'autorisation
+const authUrl = client.generateAuthUrl({
+  access_type: 'offline',
+  scope: client.scope,
+});
+
+// Rediriger l'utilisateur vers l'URL d'autorisation
+console.log('Autorisez cette application en visitant cette URL :', authUrl);

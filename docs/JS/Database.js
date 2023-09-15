5b7d27f43648cd5ba31a1a9dbb9066d4f04e83c3
KevinGacon <71588720+Raynko@users.noreply.github.com>
2023-09-15 12:03:42 +0200

    up

--- docs/JS/Database.js
+++ docs/JS/Database.js
@@ -1,93 +1,83 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCu2bExV4bdbSrB11UAxDIP3p-G9FWSj9o",
-  authDomain: "mods-manager.firebaseapp.com",
-  databaseURL: "https://mods-manager-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "mods-manager",
-  storageBucket: "mods-manager.appspot.com",
-  messagingSenderId: "32145566432",
-  appId: "1:32145566432:web:f1902aa8d286f49e88be85"
-};
+// Définissez la clé d'API
+const API_KEY = "AIzaSyBt9lVYDzQlZejxBazEUWQ5cpFoiwIipFc";
 
-// Initialisez Firebase
-firebase.initializeApp(firebaseConfig);
+// Chargez la bibliothèque auth2 de l'API Google
+gapi.load('auth2', function() {
+  // Initialisez la bibliothèque auth2 avec l'ID client OAuth
+  gapi.auth2.init({
+    client_id: '702805947096-frr4g6bhsesi47rttemrop3g6tqonbe7.apps.googleusercontent.com',
+  }).then(function() {
+    // Lorsque la bibliothèque auth2 est initialisée, vous pouvez gérer l'autorisation OAuth
+    const auth2 = gapi.auth2.getAuthInstance();
 
-// Référence à la base de données
-const modsManagerDB = firebase.database().ref('ModsManager');
+    // Fonction pour gérer l'authentification de l'utilisateur
+    function handleAuth() {
+      auth2.signIn().then(function() {
+        // L'utilisateur est authentifié, vous pouvez maintenant accéder à la feuille Google Sheets.
+        // Vous devrez utiliser le jeton d'accès OAuth pour effectuer des requêtes à l'API Sheets.
+        // Vous pouvez ajouter votre code d'accès à Sheets ici.
+      }).catch(function(error) {
+        // Gérez les erreurs d'authentification ici, par exemple affichez un message à l'utilisateur.
+        console.error('Erreur d\'authentification :', error);
+      });
+    }
 
-// Écouteur pour soumettre le formulaire
-document.getElementById('new-mod-form').addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  const category = getElementVal('cat-field');
-  const image = getElementVal('image-field');
-  const name = getElementVal('name-field');
-  const type = getElementVal('type-field');
-  const desc = getElementVal('desc-field');
-  const version = getElementVal('version-field');
-  const link = getElementVal('link-field');
-
-  saveNewMod(category, image, name, type, desc, version, link);
-
-  // Afficher l'alerte
-  document.querySelector('.alert').style.display = "block";
-
-  // Cacher l'alerte après 3 secondes
-  setTimeout(() => {
-    document.querySelector('.alert').style.display = "none";
-  }, 3000);
-
-  // Réinitialiser le formulaire
-  document.getElementById('new-mod-form').reset();
+    // Associez la fonction de gestion de l'authentification à un bouton ou un événement de votre choix.
+    // Par exemple, lorsque l'utilisateur clique sur un bouton "Se connecter".
+    const loginButton = document.getElementById('login-button');
+    loginButton.addEventListener('click', handleAuth);
+  });
 });
 
-// Fonction pour sauvegarder un nouveau mod
-const saveNewMod = (category, image, name, type, desc, version, link) => {
-  const newModForm = modsManagerDB.push();
-
-  newModForm.set({
-    category: category,
-    image: image,
-    name: name,
-    type: type,
-    desc: desc,
-    version: version,
-    link: link
-  });
-};
-
-// Fonction pour obtenir la valeur d'un élément par son ID
-const getElementVal = (id) => {
-  return document.getElementById(id).value;
-};
-
-// Fonction pour afficher les éléments depuis Firebase dans la liste
-const displayElementsFromFirebase = () => {
-  const elementList = document.getElementById('element-list');
-
-  // Utilisez la méthode "on" pour écouter les modifications continues
-  modsManagerDB.on('value', (snapshot) => {
-    elementList.innerHTML = ''; // Effacez le contenu précédent de la liste
-
-    snapshot.forEach((childSnapshot) => {
-      const data = childSnapshot.val();
-
-      // Créez un élément div pour chaque donnée de Firebase
-      const element = document.createElement('div');
-      element.classList.add('cat-content', 'element');
-      element.innerHTML = `
-        <div class="cell"><img class="element-image" src="${data.image}" onerror="this.src='IMG/blankPic.png'"></div>
-        <div class="cell element-name">${data.name}</div>
-        <div class="cell element-type">${data.type}</div>
-        <div class="cell element-desc">${data.desc}</div>
-        <div class="cell element-version">${data.version}</div>
-        <div class="cell"><a target="_blank" href="${data.link}" class="element-link">CurseForge</a></div>
-      `;
-
-      // Ajoutez l'élément à la liste
-      elementList.appendChild(element);
+gapi.load('client:auth2', function() {
+    gapi.client.init({
+      apiKey: 'AIzaSyBt9lVYDzQlZejxBazEUWQ5cpFoiwIipFc',
+      clientId: '702805947096-frr4g6bhsesi47rttemrop3g6tqonbe7.apps.googleusercontent.com',
+      discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+      scope: 'https://www.googleapis.com/auth/spreadsheets',
+    }).then(function() {
+      // L'initialisation est terminée, vous pouvez maintenant effectuer des opérations sur Google Sheets.
     });
   });
-};
 
-// Appelez cette fonction pour afficher les éléments dès que la page se charge
-window.addEventListener('load', displayElementsFromFirebase);
+// Fonction pour écrire des données dans Google Sheets
+function writeToSheet(values) {
+  // ID de la feuille Google Sheets que vous souhaitez mettre à jour.
+  const spreadsheetId = '1-ejd-hiK_HEZKdhDRiLEXLwMcst_L9JueqO_dnGAvy4';
+
+  // Utilisez la méthode spreadsheets.values.append pour ajouter les données à la feuille.
+  gapi.client.sheets.spreadsheets.values.append({
+    spreadsheetId: spreadsheetId,
+    range: 'A1', // Indiquez la cellule de départ où vous souhaitez ajouter les données.
+    valueInputOption: 'RAW', // Vous pouvez spécifier comment les données doivent être interprétées (en brut, formules, etc.).
+    insertDataOption: 'INSERT_ROWS',
+    resource: {
+      values: [values],
+    },
+  }).then(function(response) {
+    console.log('Données ajoutées avec succès !');
+    // Réinitialisez les champs du formulaire ou affichez un message de succès à l'utilisateur.
+  }).catch(function(error) {
+    console.error('Erreur lors de l\'écriture des données :', error);
+  });
+}
+
+// Ajoutez un gestionnaire d'événements pour le formulaire.
+document.getElementById('new-mod-form').addEventListener('submit', function(event) {
+  event.preventDefault(); // Empêche le formulaire de se soumettre normalement.
+
+  // Obtenez les valeurs des champs du formulaire.
+  const cat = document.getElementById('cat-field').value;
+  const image = document.getElementById('image-field').value;
+  const name = document.getElementById('name-field').value;
+  const type = document.getElementById('type-field').value;
+  const desc = document.getElementById('desc-field').value;
+  const version = document.getElementById('version-field').value;
+  const link = document.getElementById('link-field').value;
+
+  // Créez un tableau avec les valeurs du formulaire.
+  const values = [cat, image, name, type, desc, version, link];
+
+  // Utilisez la fonction writeToSheet() pour ajouter les données à Google Sheets.
+  writeToSheet(values);
+});

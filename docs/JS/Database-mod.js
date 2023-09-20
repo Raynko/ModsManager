2169016d5a49f669aeedee6291dcb81a4a537698
KevinGacon <71588720+Raynko@users.noreply.github.com>
2023-09-20 05:42:34 +0200

    update

--- docs/JS/Database-mod.js
+++ docs/JS/Database-mod.js
@@ -1,4 +1,29 @@
 const urlParams = new URLSearchParams(window.location.search);
 const modpackName = Array.from(urlParams.keys())[0];
 
-console.log("Modpack Name:", modpackName);
+// Récupérez une référence à la base de données Firebase avec le nom du modpack
+const modpackRef = firebase.database().ref('Modpacks/' + modpackName);
+
+// Sélectionnez le conteneur pour les mods
+const modsContainer = document.getElementById('element-list');
+
+// Gérez les modifications dans le nœud "Mods"
+modpackRef.child('Mods').on('child_added', (snapshot) => {
+    // Obtenir les données du mod à partir de Firebase
+    const modData = snapshot.val();
+
+    // Créez un nouvel élément mod basé sur le modèle
+    const modElement = document.getElementById('element-template').cloneNode(true);
+    modElement.removeAttribute('style'); // Afficher l'élément
+
+    // Mettez à jour les éléments du modèle avec les données du mod
+    modElement.querySelector('.element-image').src = modData.image || 'IMG/blankPic.png';
+    modElement.querySelector('.element-name').textContent = modData.name;
+    modElement.querySelector('.element-type').textContent = modData.type;
+    modElement.querySelector('.element-desc').textContent = modData.description;
+    modElement.querySelector('.element-version').textContent = modData['version-installed'];
+    modElement.querySelector('.element-link').href = modData.link;
+
+    // Ajoutez l'élément mod au conteneur des mods
+    modsContainer.appendChild(modElement);
+});

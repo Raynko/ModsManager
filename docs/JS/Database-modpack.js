76af88bb2a0ed60a632b8c1d2c4c5a99cc1794f7
KevinGacon <71588720+Raynko@users.noreply.github.com>
2023-09-20 02:40:27 +0200

    update

--- docs/JS/Database-modpack.js
+++ docs/JS/Database-modpack.js
@@ -39,16 +39,39 @@ form.addEventListener('submit', (e) => {
 
     // Vérifiez si le nom du modpack est unique (ajoutez votre logique ici)
 
-    // Ajoutez les données à la base de données Firebase sous "Modpacks"
+    // Obtenez les valeurs des cases à cocher
+    const categoryChecked = document.getElementById('chk-category').checked;
+    const imageChecked = document.getElementById('chk-image').checked;
+    const nameChecked = document.getElementById('chk-name').checked;
+    const typeChecked = document.getElementById('chk-type').checked;
+    const descriptionChecked = document.getElementById('chk-description').checked;
+    const installedVersionChecked = document.getElementById('chk-installed-version').checked;
+    const modVersionChecked = document.getElementById('chk-mod-version').checked;
+    const linkChecked = document.getElementById('chk-link').checked;
+
+    // Ajoutez les données à la base de données Firebase sous "Modpacks" avec les cases à cocher
     const newModpackRef = database.ref(`Modpacks/${name}/Settings`);
     newModpackRef.set({
         Author: author,
-        Version: version
+        Version: version,
+        Section: {
+            category: categoryChecked,
+            image: imageChecked,
+            name: nameChecked,
+            type: typeChecked,
+            description: descriptionChecked,
+            'version-installed': installedVersionChecked,
+            'mod-version': modVersionChecked,
+            link: linkChecked
+        }
     });
 
     // Réinitialisez le formulaire après l'ajout
     form.reset();
+
+    HideNewModpackForm();
 });
+
 
 // Fonction pour afficher les modpacks depuis Firebase sur la page
 function displayModpacks() {

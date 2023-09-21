8c29c8e30d29f3c61a5b759058767e31b9bca25c
KevinGacon <71588720+Raynko@users.noreply.github.com>
2023-09-21 14:11:46 +0200

    PARFAIT

--- docs/JS/Database-mod.js
+++ docs/JS/Database-mod.js
@@ -15,31 +15,35 @@ categoriesRef.once('value', (snapshot) => {
 
     // Bouclez sur chaque catégorie
     categories.forEach((category) => {
-        // Vérifiez si le modpack possède des mods dans cette catégorie
-        modpackRef.child('Mods').orderByChild('category').equalTo(category).once('value', (modsSnapshot) => {
-            if (modsSnapshot.exists()) {
-                // Créez un nouvel élément de catégorie basé sur le modèle
-                const categoryElement = document.querySelector('#category-template').cloneNode(true);
-                categoryElement.removeAttribute('style'); // Afficher l'élément
+        // Créez un nouvel élément de catégorie basé sur le modèle
+        const categoryElement = document.querySelector('#category-template').cloneNode(true);
+        categoryElement.removeAttribute('style'); // Afficher l'élément
 
-                // Mettez à jour le titre de la catégorie
-                categoryElement.querySelector('.list-cat-title').textContent = category;
+        // Mettez à jour le titre de la catégorie
+        categoryElement.querySelector('.list-cat-title').textContent = category;
 
-                // Sélectionnez le conteneur pour les mods de cette catégorie
-                const modsContainer = categoryElement.querySelector('.list-content');
+        // Sélectionnez le conteneur pour les mods de cette catégorie
+        const modsContainer = categoryElement.querySelector('.list-content');
 
-                // Gérez les modifications dans le nœud "Mods" pour cette catégorie
-                modsSnapshot.forEach((modSnapshot) => {
-                    // Obtenir les données du mod à partir de Firebase
-                    const modData = modSnapshot.val();
+        // Récupérez une référence aux mods du modpack actuel dans cette catégorie
+        const modsInCategoryRef = modpackRef.child('Mods');
 
+        modsInCategoryRef.once('value', (modsSnapshot) => {
+            let categoryHasMods = false; // Indicateur pour vérifier s'il y a des mods dans la catégorie
+
+            modsSnapshot.forEach((modSnapshot) => {
+                // Obtenir les données du mod à partir de Firebase
+                const modData = modSnapshot.val();
+
+                // Vérifier si le mod appartient à la catégorie actuelle
+                if (modData.category === category) {
                     // Créez un nouvel élément mod basé sur le modèle
                     const modElement = document.querySelector('#element-template').cloneNode(true);
                     modElement.removeAttribute('style'); // Afficher l'élément
 
                     // Mettez à jour les éléments du modèle avec les données du mod
                     modElement.querySelector('.element-image').src = modData.image || 'IMG/blankPic.png';
-                    modElement.querySelector('.element-name').textContent = modData.name;
+                    modElement.querySelector('.element-name').textContent = modSnapshot.key;
                     modElement.querySelector('.element-type').textContent = modData.type;
                     modElement.querySelector('.element-desc').textContent = modData.description;
                     modElement.querySelector('.element-version').textContent = modData['version-installed'];
@@ -47,9 +51,14 @@ categoriesRef.once('value', (snapshot) => {
 
                     // Ajoutez l'élément mod au conteneur des mods de cette catégorie
                     modsContainer.appendChild(modElement);
-                });
 
-                // Ajoutez l'élément de catégorie au conteneur global des mods
+                    // Indiquez qu'il y a des mods dans la catégorie
+                    categoryHasMods = true;
+                }
+            });
+
+            // Ajoutez l'élément de catégorie au conteneur global des mods seulement s'il y a des mods dans la catégorie
+            if (categoryHasMods) {
                 elementListContainer.appendChild(categoryElement);
             }
         });

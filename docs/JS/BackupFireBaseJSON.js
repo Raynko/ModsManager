7ca06e5ef107a45384fbfb16ab86f6f58ef7053d
KevinGacon <71588720+Raynko@users.noreply.github.com>
2023-10-08 00:45:39 +0200

    Update

--- docs/JS/BackupFireBaseJSON.js
+++ docs/JS/BackupFireBaseJSON.js
@@ -1,29 +1,30 @@
 // Lorsque le bouton est cliqué
 document.getElementById('board-backup-btn').addEventListener('click', function () {
-    // Reference to your Realtime Database data
-    const database = firebase.database().ref('/');
-  
+    // Référence à votre base de données Firebase Realtime
+    const database = firebase.database();
+    
     // Générer un nom de fichier unique basé sur le timestamp
-    const backupFileName = `backup_${moment().format('YYYYMMDD_HHmmss')}.json`;
-  
-    // Télécharger JSON data
-    database.once('value', (snapshot) => {
-      const jsonData = JSON.stringify(snapshot.val());
-  
-      // Créer un objet Blob à partir des données JSON
-      const blob = new Blob([jsonData], { type: 'application/json' });
-  
-      // Créer un lien d'ancrage pour le téléchargement
-      const downloadLink = document.createElement('a');
-      downloadLink.href = URL.createObjectURL(blob);
-      downloadLink.download = backupFileName;
-  
-      // Cliquez sur le lien pour démarrer le téléchargement
-      downloadLink.click();
-    });
-  });
+    const backupFileName = `Backup ${moment().format('(DD-MM-YYYY HH:mm:ss)')}.json`;
+    
+    // Récupérer les données JSON de la base de données
+    database.ref('/').once('value')
+      .then((snapshot) => {
+        const jsonData = JSON.stringify(snapshot.val());
 
-  
-  
-  
-  
+        // Référence au stockage Firebase
+        const storageRef = firebase.storage().ref();
+
+        // Créer une référence au fichier dans le stockage Firebase
+        const backupFileRef = storageRef.child(backupFileName);
+
+        // Mettre le contenu JSON dans le fichier
+        return backupFileRef.putString(jsonData, 'raw');
+      })
+      .then((snapshot) => {
+        console.log('Fichier JSON sauvegardé dans le stockage Firebase');
+        // Vous pouvez effectuer d'autres actions ici si nécessaire
+      })
+      .catch((error) => {
+        console.error('Erreur lors de la sauvegarde du fichier JSON dans le stockage Firebase :', error);
+      });
+  });

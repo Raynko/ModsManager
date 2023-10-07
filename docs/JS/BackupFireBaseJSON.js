f7aa50482219f5bee52adea4717ced3c993d99ad
KevinGacon <71588720+Raynko@users.noreply.github.com>
2023-10-08 00:30:16 +0200

    update

--- docs/JS/BackupFireBaseJSON.js
+++ docs/JS/BackupFireBaseJSON.js
@@ -1,44 +1,29 @@
-const admin = require('firebase-admin');
-const fs = require('fs');
-
-// Initialize Firebase Admin SDK with your service account key
-const serviceAccount = require('path/to/serviceAccountKey.json');
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://mods-manager-default-rtdb.europe-west1.firebasedatabase.app',
-  storageBucket: 'mods-manager.appspot.com',
-});
-
-// Reference to your Realtime Database data
-const database = admin.database().ref('/');
-
-// Download JSON data and save it as a local file
-database.once('value', (snapshot) => {
-  const jsonData = JSON.stringify(snapshot.val());
-  fs.writeFileSync('local_file.json', jsonData);
-  console.log('JSON data downloaded and saved as local_file.json');
-
-  // Reference to Firebase Storage bucket
-  const bucket = admin.storage().bucket();
-
-  // Specify the local path to the JSON file
-  const localFilePath = 'local_file.json';
-
-  // Specify the destination path in Firebase Storage
-  const storageFilePath = 'Mods-Manager Backup/';
-
-  // Upload the local file to Firebase Storage
-  bucket.upload(localFilePath, {
-    destination: storageFilePath,
-  })
-    .then(() => {
-      console.log('File uploaded to Firebase Storage');
-    })
-    .catch((error) => {
-      console.error('Error uploading file:', error);
-    });
-});
-
+// Lorsque le bouton est cliqué
 document.getElementById('board-backup-btn').addEventListener('click', function () {
-    //Backup du fichier json sur Storage firebase
-});
+    // Reference to your Realtime Database data
+    const database = firebase.database().ref('/');
+  
+    // Générer un nom de fichier unique basé sur le timestamp
+    const backupFileName = `backup_${moment().format('YYYYMMDD_HHmmss')}.json`;
+  
+    // Télécharger JSON data
+    database.once('value', (snapshot) => {
+      const jsonData = JSON.stringify(snapshot.val());
+  
+      // Créer un objet Blob à partir des données JSON
+      const blob = new Blob([jsonData], { type: 'application/json' });
+  
+      // Créer un lien d'ancrage pour le téléchargement
+      const downloadLink = document.createElement('a');
+      downloadLink.href = URL.createObjectURL(blob);
+      downloadLink.download = backupFileName;
+  
+      // Cliquez sur le lien pour démarrer le téléchargement
+      downloadLink.click();
+    });
+  });
+
+  
+  
+  
+  

8a074fde5119f9561b4f4bfd6effb239f057ebfa
KevinGacon <71588720+Raynko@users.noreply.github.com>
2023-10-08 03:00:19 +0200

    update

---
+++ docs/JS/AlterScripts.js
@@ -0,0 +1,9 @@
+var confirmDeletePanel = document.getElementById('delete-confirm-background');
+
+function HideDeleteConfirmPanel() {
+    confirmDeletePanel.style.display = "none";
+}
+
+function ShowDeleteConfirmPanel() {
+    confirmDeletePanel.style.display = "block";
+}
